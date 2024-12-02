204bfa20d1d0840a0a29f889dd6feb0b5eb46142
Pankaj solanki <ps4066367@gmail.com>
2024-12-02 08:36:17 -0800

    video controller added

---
+++ src/controllers/video.controller.js
@@ -0,0 +1,34 @@
+import mongoose from "mongoose";
+import {video} from "../models/video.model.js";
+import {user} from "../models/user.model.js";
+import {ApiResponse} from "../utils/ApiResponse.js";
+import {ApiError} from "../utils/ApiError.js";
+import {uploadOnCloudinary} from "../utils/cloudinary.js";
+import {asyncHandler} from "../utils/asyncHandler.js";
+
+const getAllVideos = asyncHandler(async (req,res)->{
+    const {page = 1, limit = 10, query,sortBy,sortType,userId} = req.query;
+    // get all videos based on query , sorting and pagination
+    const videos = await videos.find(query).sort({[sortBy]:sortType}).skip((page-1)*limit).limit(limit);
+
+    res.status(200).json(new ApiResponse(200,videos))
+
+})
+
+const publishVideo = asyncHandler(async (req,res=>{
+    const {title, description} = req.body;
+    // get videos , upload video to cloudinary, create video object and save it to db
+    const video = new videos({
+        title,
+        description,
+        videoFile: await uploadOnCloudinary(req.file.path)
+    })
+
+    await video.save();
+    res.status(200).json(new ApiResponse(200,video))
+}))
+
+export {
+    getAllVideos,
+    publishVideo
+}
